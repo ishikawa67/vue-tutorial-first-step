7fce9c3a33efce4a208427b57c088c856a467262
NSW-G\NSW00_909087 <masaki.ishikawa@nsw.co.jp>
2025-05-02 09:53:12 +0900

    display pushed counts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,34 +2,37 @@
   <div class="hello">
     <h1>{{ message }}</h1>
     <p>{{ description }}</p>
-    <button @click="changeMessage">Click Me</button>
+    <button @click="changeMessage">Push Please</button>
+    <p>Button pushed: {{ count }} times</p>
   </div>
 </template>
 
 <script setup lang="ts">
 // Propsの型定義
 interface Props {
-  description: string;
+  description: string
 }
 
 // Propsを定義
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 
 // リアクティブな状態の定義
-import { ref } from 'vue';
-const message = ref<string>('Hello, Vue 3 with <script setup> and TypeScript!');
+import { ref } from 'vue'
+const message = ref<string>('Hello, Vue 3 with <script setup> and TypeScript!')
+const count = ref<number>(0)
 
 // メソッドの定義
-let changeFlag = true;
+let changeFlag = true
 const changeMessage = (): void => {
+  count.value++
   if (changeFlag) {
-    message.value = 'You clicked the button! firstTime or odd';
-    changeFlag = !changeFlag;
+    message.value = 'You clicked the button! firstTime or odd'
+    changeFlag = !changeFlag
   } else {
-    message.value = 'You clicked the button! even times';
-    changeFlag = !changeFlag;
+    message.value = 'You clicked the button! even times'
+    changeFlag = !changeFlag
   }
-};
+}
 </script>
 
 <style scoped>
